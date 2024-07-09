f30d335bb4414b0075b5af6fed1f0a15cce14a4f
pretty128guydev <pretty128guydev@outlook.com>
2024-07-10 06:29:10 +0700

    push the build file due to node version confliction

---
+++ build/precache-manifest.abc62eae1ea73b22163f9068e6f843e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29652ea18122a65bb28fc7818f1c8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d50c0939a9ba1200ad",
+    "url": "/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "6e60290e75435bb009b5",
+    "url": "/static/css/main.df700d1f.chunk.css"
+  },
+  {
+    "revision": "a1d50c0939a9ba1200ad",
+    "url": "/static/js/2.20ed9656.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.20ed9656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e60290e75435bb009b5",
+    "url": "/static/js/main.fd47ced6.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
